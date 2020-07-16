a2b18e0fb327afc6530b679a824282ecc88e8594
Mr. Karaaslan <developerkaraaslan@gmail.com>
2020-07-16 16:44:18 +0300

    Initial Commit

---
+++ ituEvent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ituEvent
+//
+//  Created by Mehmet Karaaslan on 16.07.2020.
+//  Copyright © 2020 Mehmet Karaaslan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
